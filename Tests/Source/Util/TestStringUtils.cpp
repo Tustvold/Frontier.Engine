2020111f255d43e8d6ac00b98a9f0e7e5793745b
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-08-01 00:49:06 +0100

    Fixed TestStringUtils

--- Tests/Source/Util/TestStringUtils.cpp
+++ Tests/Source/Util/TestStringUtils.cpp
@@ -6,10 +6,10 @@ TEST(TestStringUtils, TestConversion) {
     const std::string a = "Foo";
     const std::wstring wa = L"Foo";
 
-    auto converted = FTStringUtil<char>::convertString(wa);
+    auto converted = FTCharUtil::convertString(wa);
     EXPECT_EQ(converted, a);
 
-    auto converted2 = FTStringUtil<wchar_t>::convertString(a);
+    auto converted2 = FTWCharUtil::convertString(a);
     EXPECT_EQ(converted2, wa);
 }
 
@@ -17,10 +17,10 @@ TEST(TestStringUtils, TestConversionLong) {
     const std::string a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.";
     const std::wstring wa = L"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.";
 
-    auto converted = FTStringUtil<char>::convertString(wa);
+    auto converted = FTCharUtil::convertString(wa);
     EXPECT_EQ(converted, a);
 
-    auto converted2 = FTStringUtil<wchar_t>::convertString(a);
+    auto converted2 = FTWCharUtil::convertString(a);
     EXPECT_EQ(converted2, wa);
 }
 
@@ -28,8 +28,8 @@ TEST(TestStringUtils, TestToLowerCase) {
     std::string a = "LfgewfJUsfSnfhgKedfhS";
     std::wstring b = L"LfgewfJUsfSnfhgKedfhS";
 
-    FTStringUtil<char>::toLowerCase(a);
-    FTStringUtil<wchar_t>::toLowerCase(b);
+    FTCharUtil::toLowerCase(a);
+    FTWCharUtil::toLowerCase(b);
 
     EXPECT_EQ(a, "lfgewfjusfsnfhgkedfhs");
     EXPECT_EQ(b, L"lfgewfjusfsnfhgkedfhs");
@@ -39,8 +39,8 @@ TEST(TestStringUtils, TestReplace) {
     std::string a = "asd\n dkfks grfesasd\nghs djgfjgawjx";
     std::wstring b = L"asd\n dkfks grfesasd\nghs djgfjgawjx";
 
-    FTStringUtil<char>::replaceAll(a, "asd\n", "foo");
-    FTStringUtil<wchar_t>::replaceAll(b, L"asd\n", L"foo");
+    FTCharUtil::replaceAll(a, "asd\n", "foo");
+    FTWCharUtil::replaceAll(b, L"asd\n", L"foo");
 
     EXPECT_EQ(a, "foo dkfks grfesfooghs djgfjgawjx");
     EXPECT_EQ(b, L"foo dkfks grfesfooghs djgfjgawjx");
@@ -51,8 +51,8 @@ TEST(TestStringUtils, TestSplit) {
     std::string a = "asda,gusef, sadf,y \n,sdfsd";
     std::wstring b = L"asda,gusef, sadf,y \n,sdfsd";
 
-    auto av = FTStringUtil<char>::splitString(a, ',');
-    auto bv = FTStringUtil<wchar_t>::splitString(b, ',');
+    auto av = FTCharUtil::splitString(a, ',');
+    auto bv = FTWCharUtil::splitString(b, ',');
 
     std::vector<std::string> av_correct = { "asda","gusef"," sadf","y \n","sdfsd" };
     std::vector<std::wstring> bv_correct = { L"asda",L"gusef",L" sadf",L"y \n",L"sdfsd" };
