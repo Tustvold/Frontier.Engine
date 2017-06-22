11336e2d81dc242738d8cef21672be91dbdd62c5
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2017-06-22 01:04:05 +0100

    Serializer template to allow serialization without modifying struct

--- Source/Util/Serialization/Serialization.h
+++ Source/Util/Serialization/Serialization.h
@@ -2,6 +2,33 @@
 
 #include "TTag.h"
 
+namespace detail {
+    template<typename T>
+    struct has_serialize {
+        struct dummy { /* something */ };
+
+        template<typename C, typename P>
+        static auto test(P *p) -> decltype(std::declval<C>().serialize(*p), std::true_type());
+
+        template<typename, typename>
+        static std::false_type test(...);
+
+        typedef decltype(test<T, dummy>(nullptr)) type;
+        static const bool value = std::is_same<std::true_type, decltype(test<T, dummy>(nullptr))>::value;
+    };
+}
+
+template<typename Archiver, typename T>
+struct Serializer {
+    static_assert(detail::has_serialize<T>::value,
+                  "Doesn't implement serialize - either add serialize member function of provide Serializer specialization");
+
+    static void serialize(Archiver &a, T &val) {
+        val.serialize(a);
+    }
+};
+
+
 class OutputSerializer {
     ttvfs::File *file;
 public:
@@ -17,7 +44,7 @@ public:
 
     template<typename T, typename std::enable_if<!detail::TagID<T>::value, int>::type = 0>
     void operator&(T &value) {
-        value.serialize(*this);
+        Serializer<OutputSerializer, T>::serialize(*this, value);
     }
 };
 
@@ -36,6 +63,6 @@ public:
 
     template<typename T, typename std::enable_if<!detail::TagID<T>::value, int>::type = 0>
     void operator&(T &value) {
-        value.serialize(*this);
+        Serializer<InputSerializer, T>::serialize(*this, value);
     }
 };
--- Tests/Source/Util/TestSerialization.cpp
+++ Tests/Source/Util/TestSerialization.cpp
@@ -71,3 +71,77 @@ TEST(TestBTF, TestNewAPI) {
         EXPECT_EQ(t2.foobar[el.first], el.second);
     }
 }
+
+
+struct TestStruct2 {
+    std::vector<std::vector<int>> foo;
+    std::vector<std::string> bar;
+    std::unordered_map<int, std::string> foobar;
+
+    template<typename T>
+    void serialize(T &s) {
+
+    }
+};
+
+template <typename Archiver>
+struct Serializer <Archiver, TestStruct2> {
+    static void serialize(Archiver &s, TestStruct2 &val) {
+        s & val.foo;
+        s & val.bar;
+        s & val.foobar;
+    }
+};
+
+TEST(TestBTF, TestNewAPI2) {
+    MockLoader loader;
+
+    ttvfs::File *fp = FTEngine::getFileManager()->getOrCreateFile("Test.hex");
+    fp->open("wb");
+
+    const int num_tests = 1000;
+
+    TestStruct2 t;
+
+    std::vector<int> a;
+    std::vector<int> b;
+    for (int i = 0; i < num_tests; i++) {
+        a.push_back(rand());
+        b.push_back(rand());
+    }
+
+    t.foo.emplace_back(std::move(a));
+    t.foo.emplace_back(std::move(b));
+
+    t.foobar[rand()] = "asdf";
+    t.foobar[rand()] = "asdfgs";
+    t.foobar[rand()] = "afsghjwsjs";
+
+    t.bar = {"Hello", "Helloasd", "Helloasfdg"};
+
+    OutputSerializer out(fp);
+    out & t;
+
+    fp->close();
+
+    fp->open("rb");
+
+    InputSerializer in(fp);
+
+    TestStruct2 t2;
+
+    in & t2;
+
+    for (int i = 0; i < num_tests; i++) {
+        EXPECT_EQ(t.foo[0][i],t2.foo[0][i]);
+        EXPECT_EQ(t.foo[1][i],t2.foo[1][i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        EXPECT_EQ(t.bar[i], t2.bar[i]);
+    }
+
+    for (auto& el : t.foobar) {
+        EXPECT_EQ(t2.foobar[el.first], el.second);
+    }
+}
